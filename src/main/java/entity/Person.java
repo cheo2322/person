d527304355d7b5644d33f1cd942a8fc12b3b17f3
cheo2322 <barcecheo2319@gmail.com>
2025-03-12 12:55:35 -0500

    Person entities

---
+++ src/main/java/entity/Person.java
@@ -0,0 +1,27 @@
+package entity;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
+public class Person {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    private Gender gender;
+    private Integer age;
+    private String identification;
+    private String address;
+    private String phone;
+
+    enum Gender {
+        MALE,
+        FEMALE,
+        OTHER
+    }
+}
