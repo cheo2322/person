a4bf9e158d69fbde90e8f078f166efebc29e4084
cheo2322 <barcecheo2319@gmail.com>
2025-03-14 16:30:31 -0500

    Database first arguments

--- src/main/java/com/devsu/person/entity/Client.java
+++ src/main/java/com/devsu/person/entity/Client.java
@@ -1,23 +1,30 @@
 package com.devsu.person.entity;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
 import lombok.Getter;
 import lombok.Setter;
-
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
 
 @Entity
 @Setter
 @Getter
-public class Client extends Person {
+public class Client {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @Column(unique = true)
     private String clientId;
     private String password;
-    private String status;
+    private Boolean status;
+
+    @ManyToOne
+    @JoinColumn(name = "person_identification", referencedColumnName = "identification")
+    private Person person;
 }
--- src/main/java/com/devsu/person/entity/Person.java
+++ src/main/java/com/devsu/person/entity/Person.java
@@ -1,27 +1,40 @@
 package com.devsu.person.entity;
 
-import javax.persistence.Entity;
-import javax.persistence.GeneratedValue;
-import javax.persistence.GenerationType;
-import javax.persistence.Id;
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
 
 @Entity
+@Setter
+@Getter
 public class Person {
 
-    @Id
-    @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
 
-    private String name;
-    private Gender gender;
-    private Integer age;
-    private String identification;
-    private String address;
-    private String phone;
+  private String name;
 
-    enum Gender {
-        MALE,
-        FEMALE,
-        OTHER
-    }
+  @Enumerated(EnumType.STRING)
+  private Gender gender;
+
+  private Integer age;
+
+  @Column(unique = true)
+  private String identification;
+
+  private String address;
+  private String phone;
+
+  enum Gender {
+    MALE,
+    FEMALE,
+    OTHER
+  }
 }
